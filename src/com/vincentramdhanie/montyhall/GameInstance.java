ae7666a7eab17f6137fe8a353692e229e4d3e15c
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-12-02 10:44:24 -0400

    Update GameInstance

--- src/com/vincentramdhanie/montyhall/GameInstance.java
+++ src/com/vincentramdhanie/montyhall/GameInstance.java
@@ -3,67 +3,77 @@ package com.vincentramdhanie.montyhall;
 import java.util.Random;
 
 /**
- *
+ * Represents an instance of a Game
+ * 
  * @author Vincent Ramdhanie
  */
 public class GameInstance {
     private int win;
     private int lose;
     private int other;
-    
+
     /**
-     * Creates a GameInstance randomly generating the 
-     * win, lose and other values
+     * Creates a GameInstance randomly generating the win, lose and other values
      */
-    public GameInstance(){
+    public GameInstance() {
         Random ran = new Random();
         win = ran.nextInt(3) + 1;
         int a = 0, b = 0;
-        switch(win){
-            case 1:a = 2; b = 3;break;
-            case 2:a = 1; b = 3;break;
-            case 3:a = 1; b = 2;break;
+        switch (win) {
+        case 1:
+            a = 2;
+            b = 3;
+            break;
+        case 2:
+            a = 1;
+            b = 3;
+            break;
+        case 3:
+            a = 1;
+            b = 2;
+            break;
         }
-        if(ran.nextDouble() < 0.5){
+        if (ran.nextDouble() < 0.5) {
             lose = a;
             other = b;
-        }else{
+        } else {
             other = a;
             lose = b;
         }
     }
-    
+
     /**
      * Creates a GameInstance with a preset win, lose, other
+     * 
      * @param win
      * @param lose
-     * @param other 
+     * @param other
      */
-    public GameInstance(int win, int lose, int other){
+    public GameInstance(int win, int lose, int other) {
         setWin(win);
         setLose(lose);
         setOther(other);
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("Winning Door:%d, Other Doors:%d,%d", getWin(), getLose(), getOther());
     }
 
-    public int findOtherNonWinningDoor(int choice){
+    public int findOtherNonWinningDoor(int choice) {
         int ans = 0;
-        if(choice == win){
+        if (choice == win) {
             ans = other;
-        }else{
-            if(choice == lose){
+        } else {
+            if (choice == lose) {
                 ans = win;
-            }else{
+            } else {
                 ans = win;
             }
         }
         return ans;
     }
-    
+
     /**
      * @return the win
      */
